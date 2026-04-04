3311e5f753af8209277b0c1a690de7a2346c71f4
highkeylearning <hazellockey@icloud.com>
2026-04-04 16:08:52 +0000

    Deploying to gh-pages from @ highkeylearning/highkeylearning.github.io@8b90b308c9f5300a2b8997329c5f2beeddc20a04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,17 +59,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/tabs/";
           
         },
-      },{id: "post-a-post-that-can-be-cited",
-        
-          title: "a post that can be cited",
-        
-        description: "this is what a post that can be cited looks like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/post-citation/";
-          
-        },
       },{id: "post-a-post-with-pseudo-code",
         
           title: "a post with pseudo code",
