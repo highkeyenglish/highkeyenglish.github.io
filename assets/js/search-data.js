ae8bb6b3f0effdbb664568c7470ca24d6270254a
highkeylearning <hazellockey@icloud.com>
2026-04-04 16:20:50 +0000

    Deploying to gh-pages from @ highkeylearning/highkeylearning.github.io@d5715c7827ccb705ae3223330d4050a94c399bc2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -179,6 +179,11 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-macbeth",
           title: 'Macbeth',
           description: "",
