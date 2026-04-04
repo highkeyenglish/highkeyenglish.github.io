e5b3391360ca6b51b4a943cc0b11d65031ebd226
highkeyenglish <hazellockey@icloud.com>
2026-04-04 22:54:32 +0000

    Deploying to gh-pages from @ highkeyenglish/highkeyenglish.github.io@374d203232a5c123aa83c35876338e8e78db60d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,6 +63,17 @@ ninja.data = [{
             window.location.href = "/blog/2024/advanced-images/";
           
         },
+      },{id: "post-a-post-with-jupyter-notebook",
+        
+          title: "a post with jupyter notebook",
+        
+        description: "an example of a blog post with jupyter notebook",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/jupyter-notebook/";
+          
+        },
       },{id: "post-masculinity-in-macbeth",
         
           title: "Masculinity in Macbeth",
