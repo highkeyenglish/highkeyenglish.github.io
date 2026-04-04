5ac83ff034b09a3e693a392820fb57d90f48907e
highkeyenglish <hazellockey@icloud.com>
2026-04-04 22:52:59 +0000

    Deploying to gh-pages from @ highkeyenglish/highkeyenglish.github.io@98ffcc9c55d8bc0570e329565b80f584f3f50024 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -177,6 +177,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/macbeth/";
+            },},{id: "teachings-introduction-to-machine-learning",
+          title: 'Introduction to Machine Learning',
+          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
+          section: "Teachings",handler: () => {
+              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-cv',
         title: 'CV',
