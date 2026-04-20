81e1bd69c7ac6b60fef3910685e5d799c1c28d04
highkeyenglish <hazellockey@icloud.com>
2026-04-20 22:01:45 +0000

    Deploying to gh-pages from @ highkeyenglish/highkeyenglish.github.io@e207c5167c8f0d9711eb5fec64c0589e6a31cd63 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/tutors/";
           },
-        },{id: "post-masculinity-in-39-macbeth-39-teaching-materials",
+        },{id: "post-39-macbeth-39-context-teaching-materials",
+        
+          title: "&#39;Macbeth&#39; Context ~ Teaching Materials",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/macbeth-context/";
+          
+        },
+      },{id: "post-masculinity-in-39-macbeth-39-teaching-materials",
         
           title: "Masculinity in &#39;Macbeth&#39; ~ Teaching Materials",
         
